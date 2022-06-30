5aafee38e933f9e9d39328e76a4c265f6a57ffe4
taftadjani <taftadjani@gmail.com>
2022-06-30 08:26:11 +0100

    Splash Screen

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'package:fitness_application/constants/app_constant.dart';
 import 'package:fitness_application/languages/app_message.dart';
 import 'package:fitness_application/services/theme_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 class App extends StatelessWidget {
@@ -12,6 +13,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Remove splash screen after initialization is completed
+    FlutterNativeSplash.remove();
+
     return GetMaterialApp(
       title: AppConstant.appName,
       debugShowCheckedModeBanner: false,
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:fitness_application/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Init get storage
   await GetStorage.init();
